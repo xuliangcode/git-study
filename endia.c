3bb08d6c43c215932813072bdab2ad69a3f52ab8
xuliang <xuliang982006@126.com>
2017-02-19 11:16:30 +0800

    add space in code add code

--- endia.c
+++ endia.c
@@ -3,10 +3,11 @@
 static union {
 	char c[4];
 	unsigned int l;
-}endia_test = {'l', '?', '?', 'b'};
+} endia_test = {'l', '?', '?', 'b'};
 
 
 int main(void)
 {
+	printf("Test git reset \n")
 	printf("This machine is %c \n", (char)endia_test.l);
 }
